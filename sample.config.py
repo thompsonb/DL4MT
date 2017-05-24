c07bfdeff3ed9548f3bed4f6f102ab108a22456a
Gaurav Kumar <gaurav.bison@gmail.com>
2017-05-24 19:02:50 -0400

    Sample config file

---
+++ sample.config.py
@@ -0,0 +1,16 @@
+# Config for a dual-learning run
+import os
+
+DATA_DIR = "runs"
+LANG_A = 'en'
+LANG_B = 'de'
+LM_A = "testLL/test_data/en.lm.zip"
+LM_B = "testLL/test_data/de.lm.zip"
+
+WMT16_SYSTEMS_DIR="/export/b09/ws15gkumar/experiments/wmt16-models/"
+MODELDIR_AB = os.path.join(WMT16_SYSTEMS_DIR, '%s-%s/'%(LANG_A, LANG_B))
+MODELDIR_BA = os.path.join(WMT16_SYSTEMS_DIR, '%s-%s/'%(LANG_A, LANG_B))
+
+PARALLEL_DATASETS=(DATA_DIR+'/corpus.'+LANG_A, DATA_DIR+'/corpus.'+LANG_B)
+MONOLINGUAL_DATASETS=(DATA_DIR+'/corpus.'+LANG_A, DATA_DIR+'/corpus.'+LANG_B) # HACK - passing in parallel
+VALID_DATASETS=('data/newsdev2016.bpe.ro', 'data/newsdev2016.bpe.en')
