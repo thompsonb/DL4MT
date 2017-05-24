0ef1fd41dbcd4553a08d14875365ec93c9ab7815
KentonMurray <kenton.murray@gmail.com>
2017-05-24 18:53:11 -0400

    updated wget command to get rico models

--- nematus/config.py
+++ nematus/config.py
@@ -8,5 +8,11 @@ PORT_NUMBER = 8091
 temp_loc = None  # None==use system default
 TEMP_DIR = None  # None==use system default
 
-# wget http://data.statmt.org/rsennrich/wmt16_systems/en-de/ -r ...
+# Create a directory for the data, then cd into it and run the en-de command
+# wget command for just en-de
+# wget -r --cut-dirs=2 -e robots=off -nH -np -R index.html* http://data.statmt.org/rsennrich/wmt16_systems/en-de/
+
+# if you wanted all of the language pairs...
+# wget -r --cut-dirs=1 -e robots=off -nH -np -R index.html* http://data.statmt.org/rsennrich/wmt16_systems/
+
 wmt16_systems_dir = '/todo/path/wmt16_Rico_systems/'
