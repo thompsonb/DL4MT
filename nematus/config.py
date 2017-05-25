46f806a214c99e045e9ca7a56f803667ffcae464
thompsonb <brian2ndary@gmail.com>
2017-05-25 11:08:25 -0400

    combined TEMP_DIR and temp_loc

--- nematus/config.py
+++ nematus/config.py
@@ -5,6 +5,5 @@ python_loc = '/path/to/python/with/lots/of/extra/packages'
 cuda_loc = '/usr/local/cuda'
 KENLM_PATH = '/todo/path/kenlm/build/'
 PORT_NUMBER = 8091
-temp_loc = None  # None==use system default
 TEMP_DIR = None  # None==use system default
 WMT16_SYSTEMS_DIR="/export/b09/ws15gkumar/experiments/wmt16-models/"
